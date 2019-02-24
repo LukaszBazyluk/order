655b416a828cd0ae59b2f79e2b2d251c7a8395e7
Henryk Bender <henryk.bender@itli.pl>
2019-02-24 12:34:00 +0100

    obsluga walidatora

--- src/main/java/Klient.java
+++ src/main/java/Klient.java
@@ -5,16 +5,25 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
 public class Klient {
 
     private String imie;
     private String nazwisko;
     private String nazwaFirmy;
-    private int nip;
+    private long nip;
     private boolean firma;
     private long pesel;
     private String adres;
 
+    public Klient(String imie, String nazwisko, long pesel, String adres) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        if (PeselValidator.valid(pesel)) {
+            this.pesel = pesel;
+        } else {
+            throw new IllegalArgumentException("Niepoprawny PESEL");
+        }
+        this.adres = adres;
+    }
 }
--- src/test/java/KlientTest.java
+++ src/test/java/KlientTest.java
@@ -15,13 +15,14 @@ public class KlientTest {
         klient.setNazwisko("Aaaa");
         klient.setPesel(12345678911l);
         //then
-        assertEquals(false,klient.isFirma());
+        assertEquals(false, klient.isFirma());
         assertEquals("Tomek", klient.getImie());
         assertEquals("Aaaa", klient.getNazwisko());
         assertEquals(12345678911l, klient.getPesel());
     }
+
     @Test
-    public void testFirma(){
+    public void testFirma() {
         //given
         Klient firma = new Klient();
         //when
@@ -29,8 +30,30 @@ public class KlientTest {
         firma.setNazwaFirmy("Kogucik");
         firma.setNip(1234567891);
         //then
-        assertEquals(true,firma.isFirma());
+        assertEquals(true, firma.isFirma());
         assertEquals("Kogucik", firma.getNazwaFirmy());
-        assertEquals(1234567891,firma.getNip());
+        assertEquals(1234567891, firma.getNip());
+    }
+
+    @Test
+    public void testKonstruktoraPoprawnyPesel() {
+        //given
+        long pesel = 27092924229L;
+        //when
+        Klient klient = new Klient("Jan", "Kowalski", pesel, "ulica Błotna");
+
+        //then
+        assertEquals(pesel, klient.getPesel());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testKonstruktoraNiePoprawnyPesel() {
+        //given
+        long pesel = 17092924229L;
+
+        //when
+        Klient klient = new Klient("Jan", "Kowalski", pesel, "ulica Błotna");
+
+        //then
     }
 }
